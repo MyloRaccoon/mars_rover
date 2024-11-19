033ec81f44c3c9a24f7370cf7f1d66a61a64e1c1
Stanislas Trillard <159046288+strillard@users.noreply.github.com>
2024-11-19 16:45:28 +0100

    add backward_command

--- backward_command.py
+++ backward_command.py
@@ -1,10 +1,10 @@
 from command import Command
 from rover import Rover
 
-class ForwardCommand(Command):
+class BackwardCommand(Command):
 
 	def __init__(self, rover: Rover):
 		self.rover = rover
 
 	def execute(self):
-		self.rover.forward()
+		self.rover.backward()
